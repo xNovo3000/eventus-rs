e0ba4c4bcc8c22dca161223c260bd07ae24ad7bf
xNovo3000 <fontana.ciro.2001@gmail.com>
2023-09-13 12:35:13 +0200

    Moving on linux

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,30 @@
-use tracing::info;
+use std::env;
+
+use diesel_async::{AsyncPgConnection, AsyncConnection};
+use dotenvy::dotenv;
+use tracing::error;
+
+pub mod dto;
+pub mod handler;
+pub mod model;
+pub mod schema;
 
 #[tokio::main(flavor = "multi_thread")]
-async fn main() {
+async fn main() -> Result<(), ()> {
+    // Load tracing
     tracing_subscriber::fmt::init();
-    info!("Hello, World!");
+    // Try to load .env file
+    if let Err(error) = dotenv() {
+        error!("Cannot load .env file: {error}");
+        return Err(())
+    }
+    // Get database URL
+    let database_url = env::var("DATABASE_URL")
+        .expect("Cannot load DATABASE_URL");
+    // Connect to the database
+    let _connection = AsyncPgConnection::establish(&database_url)
+        .await
+        .expect("Failed to connect to the database");
+    // Ok
+    Ok(())
 }
--- src/model.rs
+++ src/model.rs
@@ -1,10 +1,48 @@
+use chrono::{DateTime, Local};
 use diesel::prelude::*;
 
-#[derive(Queryable, Selectable)]
+#[derive(Queryable, Selectable, Identifiable, Debug, PartialEq)]
 #[diesel(table_name = crate::schema::user_)]
 pub struct User {
     pub id: u32,
     pub username: String,
     pub password_: String,
     pub active: bool
+}
+
+#[derive(Queryable, Selectable, Identifiable, Associations, Debug, PartialEq)]
+#[diesel(table_name = crate::schema::authority)]
+#[diesel(belongs_to(User))]
+pub struct Authority {
+    pub id: u32,
+    pub name_: String,
+    pub user_id: u32
+}
+
+#[derive(Queryable, Selectable, Identifiable, Associations, Debug, PartialEq)]
+#[diesel(table_name = crate::schema::event_)]
+#[diesel(belongs_to(User, foreign_key = creator))]
+pub struct Event {
+    pub id: u32,
+    pub name_: String,
+    pub description_: String,
+    pub creator: u32,
+    pub creation_date: DateTime<Local>,
+    pub start_: DateTime<Local>,
+    pub end_: DateTime<Local>,
+    pub seats: i32,
+    pub approved: bool
+}
+
+#[derive(Queryable, Selectable, Identifiable, Associations, Debug, PartialEq)]
+#[diesel(table_name = crate::schema::subscription)]
+#[diesel(belongs_to(User))]
+#[diesel(belongs_to(Event))]
+pub struct Subscription {
+    pub id: u32,
+    pub user_id: u32,
+    pub event_id: u32,
+    pub creation_date: DateTime<Local>,
+    pub rating: Option<i32>,
+    pub comment: Option<String>
 }
